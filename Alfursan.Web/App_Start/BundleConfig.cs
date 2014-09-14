9522f257f48739b4d1c3d203492a56973e5fea86
ToodF <fevzikaba@gmail.com>
2014-09-15 00:38:44 +0300

    Added filter column in datatables

--- Alfursan.Web/App_Start/BundleConfig.cs
+++ Alfursan.Web/App_Start/BundleConfig.cs
@@ -18,15 +18,19 @@ namespace Alfursan.Web
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
+
             bundles.Add(new ScriptBundle("~/bundles/datatable").Include(
                         "~/Plugins/Datatable/js/jquery.dataTables.min.js",
-                        "~/Plugins/Datatable/js/dataTables.bootstrap.js"
+                        "~/Plugins/Datatable/js/dataTables.bootstrap.js",
+                        "~/Plugins/Datatable/js/dataTables.responsive.js",
+                        "~/Plugins/Datatable/js/jquery-ui.min.js",
+                        "~/Plugins/Datatable/js/jquery.dataTables.columnFilter.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"
-                      //,"~/Scripts/Alfursan.Ajax.js"
+                //,"~/Scripts/Alfursan.Ajax.js"
                       ));
 
             bundles.Add(new ScriptBundle("~/bundles/fileupload").Include(
@@ -43,15 +47,21 @@ namespace Alfursan.Web
             bundles.Add(new StyleBundle("~/Style/en").Include(
                       "~/Content/en/bootstrap.css",
                       "~/Content/en/site.css",
-                      "~/Plugins/Datatable/css/jquery.dataTables.css",
-                      "~/Plugins/Datatable/css/jquery.dataTables_themeroller.css",
                       "~/Plugins/Datatable/css/style.css",
-                      "~/Plugins/Datatable/css/jquery.fileupload.css",
-                      "~/Plugins/Datatable/css/dataTables.bootstrap.css"
+                      "~/Plugins/Datatable/css/jquery.fileupload.css"
                       ));
             bundles.Add(new StyleBundle("~/Style/ar").Include(
                      "~/Content/ar/bootstrap.css",
                      "~/Content/ar/site.css"));
+
+            bundles.Add(new StyleBundle("~/Style/Datatable").Include(
+                     "~/Plugins/Datatable/css/jquery.dataTables.min.css",
+                      "~/Plugins/Datatable/css/jquery.dataTables_themeroller.css",
+                      "~/Plugins/Datatable/css/dataTables.bootstrap.css",
+                      "~/Plugins/Datatable/css/dataTables.responsive.css",
+                      "~/Plugins/Datatable/css/jquery-ui.min.css",
+                      "~/Plugins/Datatable/css/dataTables.bootstrap.css"
+                      ));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
